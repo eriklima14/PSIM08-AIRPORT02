5ffdec40ce147654bae2ae21df1336eda5044396
Android <Android@CTEI36>
2026-02-26 11:38:51 +0000

    Classe Adicionada (Modelo de Dados)

--- app/src/main/java/com/example/airport02/Flight.java
+++ app/src/main/java/com/example/airport02/Flight.java
@@ -1,19 +1,21 @@
 package com.example.airport02;
 
-public class Flight {
+public class Flight
+{
     private String origem;
     private String numeroVoo;
     private String horaPrevista;
     private String horaFinal;
 
-    public Flight(String origem, String numeroVoo, String horaPrevista, String horaFinal) {
+    public Flight(String origem, String numeroVoo, String horaPrevista, String horaFinal)
+    {
         this.origem = origem;
         this.numeroVoo = numeroVoo;
         this.horaPrevista = horaPrevista;
         this.horaFinal = horaFinal;
     }
 
-    // Getters
+    // Métodos
     public String getOrigem() { return origem; }
     public String getNumeroVoo() { return numeroVoo; }
     public String getHoraPrevista() { return horaPrevista; }
